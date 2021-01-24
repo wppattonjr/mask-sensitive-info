16b7e4e751d7074c2a9d5475bb66737c69fa5272
Wendell Patton Jr <wendell.pattonjr@gmail.com>
2021-01-24 15:55:51 -0600

    mask sensitive information exercise

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MaskSensitiveInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey you....Yeah You. Tell me a secret. I promise that I won't tell anyone.");
+            var theSecret = Console.ReadLine();
+            var maskedInfo = theSecret.Substring(theSecret.Length - 4).PadLeft(theSecret.Length, '#');
+
+            Console.WriteLine(maskedInfo);
+        }
+    }
+}
